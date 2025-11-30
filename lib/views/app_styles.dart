d5667f8d50a66fc1e054dfba04cfd3d14fd83098
swayampatnaik <swayampatnaik25@gmail.com>
2025-11-30 15:49:04 +0000

    Create app_styles.dart to manage styles for text

---
+++ lib/views/app_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const TextStyle normalText = TextStyle(
+  fontSize: 16,
+);
+
+const heading1 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const heading2 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
