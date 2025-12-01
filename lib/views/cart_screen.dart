79de00263b6c801d17a4d14b0430be2d01f0a445
swayampatnaik <swayampatnaik25@gmail.com>
2025-12-01 23:36:52 +0000

    Add header and footer to cart_screen.dart

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/views/app_styles.dart';
+import 'package:union_shop/views/header.dart';
+import 'package:union_shop/views/footer.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
+  
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Cart', style: heading1),
-      ),
-      body: const Center(
-        child: Text(
-          'Cart Content Goes Here',
-          style: normalText,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(
+              placeholderCallbackForButtons: () {},
+            ),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 125.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Your Cart', style: heading1),
+                  SizedBox(height: 40),
+                  Text(
+                    'Your cart is currently empty. Browse our products and add items to your cart to see them here.',
+                    style: normalText,
+                  ),
+                ],
+              ),
+            ),
+            // Footer (extracted)
+            Footer(
+            placeholderCallbackForButtons: placeholderCallbackForButtons,
+            ),
+          ],
         ),
       ),
     );
