62d99360859bf331cbd00398690dc745decbde1d
swayampatnaik <swayampatnaik25@gmail.com>
2025-12-01 21:55:12 +0000

    Use header.dart in about_screen.dart

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/views/app_styles.dart';
+import 'package:union_shop/views/header.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
@@ -7,19 +8,27 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('', style: heading1),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 125.0),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('About us', style: heading1),
-            SizedBox(height: 40),
-            Text(
-              'Welcome to the Union Shop!\n \nWe are dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n \nAll online purcghases are available for delivery or instore collection!\n \nWe hope you enjoy out products as much as we enjoy offering them to you. If you have any questions or comments, please dont hesitate to contact us at hello@upsu.net.\n \nHappy Shopping!\n \nThe Union Shop & Reception Team',
-              style: normalText,
+            Header(
+              onToggleMenu: () {},
+              placeholderCallbackForButtons: () {},
+            ),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 125.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('About us', style: heading1),
+                  SizedBox(height: 40),
+                  Text(
+                    'Welcome to the Union Shop!\n \nWe are dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n \nAll online purcghases are available for delivery or instore collection!\n \nWe hope you enjoy out products as much as we enjoy offering them to you. If you have any questions or comments, please dont hesitate to contact us at hello@upsu.net.\n \nHappy Shopping!\n \nThe Union Shop & Reception Team',
+                    style: normalText,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
