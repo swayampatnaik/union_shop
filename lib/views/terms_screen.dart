ca29d57b0b937ddba40dc883b5d52a6a91d16502
swayampatnaik <swayampatnaik25@gmail.com>
2025-12-01 23:42:06 +0000

    Add header and footer to terms_screen.dart

--- lib/views/terms_screen.dart
+++ lib/views/terms_screen.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/views/app_styles.dart';
+import 'package:union_shop/views/header.dart';
+import 'package:union_shop/views/footer.dart';
 
 class TermsScreen extends StatelessWidget {
   const TermsScreen({super.key});
+  
+  void placeholderCallbackForButtons() {
+    // This is the event handler for buttons that don't work yet
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Terms & Conditions of Sale Policy', style: heading1),
-      ),
-      body: const Center(
-        child: Text(
-          'Terms & Conditions of Sale Policy Content Goes Here',
-          style: normalText,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(
+              placeholderCallbackForButtons: () {},
+            ),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 75.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Terms & Conditions of Sale Policy', style: heading1),
+                  SizedBox(height: 40),
+                  Text(
+                    'Terms & Conditions of Sale Policy Content Goes Here',
+                    style: normalText,
+                  ),
+                ],
+              ),
+            ),
+            
+            Footer(
+            placeholderCallbackForButtons: placeholderCallbackForButtons,
+            ),
+          ],
         ),
       ),
     );
   }
-} 
+}
