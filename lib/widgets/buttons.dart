9e98a451c2b860534209a16278d86b811c25e6db
swayampatnaik <swayampatnaik25@gmail.com>
2025-12-02 22:17:15 +0000

    Add class for a white button with purple text in buttons.dart

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Purple button with white text
 class PurpleButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
@@ -19,6 +20,40 @@ class PurpleButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         backgroundColor: const Color(0xFF4d2963),
         foregroundColor: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        padding: padding,
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+
+/// White button with purple text
+class WhiteButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final EdgeInsets padding;
+
+  const WhiteButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.padding = const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: const Color(0xFF4d2963),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.zero,
         ),
